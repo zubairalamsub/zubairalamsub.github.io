70a6063dd744972eec6099217df6d32129100dab
zubairalamsub <35332975+zubairalamsub@users.noreply.github.com>
2018-01-25 15:23:03 +0600

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,33 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'img/mozila.jpg') {
+      myImage.setAttribute ('src','img/mozila1.jpg');
+	}
+	else {
+	  myImage.setAttribute ('src','img/mozila.jpg');
+	}
+}
+
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
