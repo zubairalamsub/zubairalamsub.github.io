7307db918ed806603cb186d1bfe163b895fcf823
Zubair Alam <35332975+zubairalamsub@users.noreply.github.com>
2021-01-06 14:27:18 +0600

    Update script.js

--- cal/script.js
+++ cal/script.js
@@ -42,25 +42,25 @@ function playSound(selector){
 
     switch (selector) {
         case 'error':
-            click.src = '/assets/sounds/error.mp3'
+            click.src = './assets/sounds/error.mp3'
             click.play();
             break;
            
         case 'correct':
-            click.src = '/assets/sounds/correct.mp3'
+            click.src = './assets/sounds/correct.mp3'
             click.play();
             break;
         case 'discard':
-            click.src = '/assets/sounds/discard.mp3'
+            click.src = './assets/sounds/discard.mp3'
             click.play();
             break;
            
         case 'clear':
-            click.src = '/assets/sounds/clear.mp3'
+            click.src = './assets/sounds/clear.mp3'
             click.play();
             break;
         default:
-            click.src = '/assets/sounds/click.mp3'
+            click.src = './assets/sounds/click.mp3'
             click.play();
             break;
     }
